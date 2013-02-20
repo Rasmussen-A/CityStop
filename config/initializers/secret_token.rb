39cab30dd80e4993754f3611a8f32f8436f7033a
Andrew <re.admit.re@gmail.com>
2013-02-20 16:55:41 +0400

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityStop::Application.config.secret_token = 'c638bdd4c65f1872c7e8378f02ab3e06991674d3d1f7c54c713d69abe4f33b121733c9f45125470fd692cb9bd569f6efde665632508e9ab134c116bc9a54babe'
